a8eff060e7e0dc309b4bbb64fa97060cfe1968c2
Andres Felipe Gañan Moreno <aganan@ADADOM.LOCAL>
2025-08-09 19:05:55 -0500

    initial

---
+++ src/main/java/com/anfega/EventStreamReactor/EventStreamReactorApplication.java
@@ -0,0 +1,13 @@
+package com.anfega.EventStreamReactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventStreamReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventStreamReactorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anfega/EventStreamReactor/EventStreamReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anfega.EventStreamReactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventStreamReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
